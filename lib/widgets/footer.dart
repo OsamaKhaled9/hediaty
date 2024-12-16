758409ae940d83c35a237acdbb5b24d64769de7c
OsamaKhaled9 <osamakhaled236@gmail.com>
2024-12-16 13:41:45 +0200

    Footer update.

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,85 +1,133 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../utils/constants.dart';
+import 'package:hediaty/screens/event_list_page.dart';
+import 'package:hediaty/screens/gift_list_page.dart';
+import 'package:hediaty/utils/constants.dart';
 
-/*class Footer extends StatelessWidget {
-  final String currentRoute;
+class Footer extends StatefulWidget {
+  @override
+  _FooterState createState() => _FooterState();
+}
 
-  Footer({required this.currentRoute});
+class _FooterState extends State<Footer> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _buttonScaleAnimation;
 
   @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: _getCurrentIndex(),
-      onTap: (index) {
-        switch (index) {
-          case 0:
-            Navigator.pushReplacementNamed(context, '/home');
-            break;
-          case 1:
-            Navigator.pushReplacementNamed(context, '/events');
-            break;
-          case 2:
-            Navigator.pushReplacementNamed(context, '/profile');
-            break;
-        }
-      },
-      items: [
-        BottomNavigationBarItem(
-          icon: FaIcon(FontAwesomeIcons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: FaIcon(FontAwesomeIcons.calendarAlt),
-          label: 'Events',
-        ),
-        BottomNavigationBarItem(
-          icon: FaIcon(FontAwesomeIcons.user),
-          label: 'Profile',
-        ),
-      ],
-      selectedItemColor: Color(0x2A6BFF),
-      unselectedItemColor: Color(0xF2F2F2),
-      showSelectedLabels: true,
-      showUnselectedLabels: true,
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _buttonScaleAnimation = Tween<double>(begin: 1.0, end: 1.2).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
     );
   }
 
-  int _getCurrentIndex() {
-    switch (currentRoute) {
-      case '/home':
-        return 0;
-      case '/events':
-        return 1;
-      case '/profile':
-        return 2;
-      default:
-        return 0;
-    }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
-}
-*/
-class Footer extends StatelessWidget {
+
+  void _playButtonAnimation() {
+    _animationController.forward().then((_) {
+      _animationController.reverse();
+      // Add sound effect here
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Color(0xFF2A6BFF);
+    final backgroundColor = Color(0xFFADD8E6);
+    final textColor = Color(0xFF333333);
+
     return BottomNavigationBar(
+      backgroundColor: Colors.white,
+      selectedItemColor: primaryColor,
+      unselectedItemColor: Colors.grey[600],
+      type: BottomNavigationBarType.fixed,
+      elevation: 0,
       items: [
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
+          icon: GestureDetector(
+            onTap: () {
+              _playButtonAnimation();
+              Navigator.pushNamed(context, '/home');
+            },
+            child: AnimatedBuilder(
+              animation: _buttonScaleAnimation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _buttonScaleAnimation.value,
+                  child: Icon(Icons.home),
+                );
+              },
+            ),
+          ),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.account_circle),
+          icon: GestureDetector(
+            onTap: () {
+              _playButtonAnimation();
+              Navigator.pushNamed(context, '/events');
+            },
+            child: AnimatedBuilder(
+              animation: _buttonScaleAnimation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _buttonScaleAnimation.value,
+                  child: Icon(FontAwesomeIcons.calendar),
+                );
+              },
+            ),
+          ),
+          label: 'Events',
+        ),
+        BottomNavigationBarItem(
+          icon: GestureDetector(
+            onTap: () {
+              _playButtonAnimation();
+              Navigator.pushNamed(context, '/gifts');
+            },
+            child: AnimatedBuilder(
+              animation: _buttonScaleAnimation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _buttonScaleAnimation.value,
+                  child: Icon(FontAwesomeIcons.gift),
+                );
+              },
+            ),
+          ),
+          label: 'Gifts',
+        ),
+        BottomNavigationBarItem(
+          icon: GestureDetector(
+            onTap: () {
+              _playButtonAnimation();
+              Navigator.pushNamed(context, '/profile');
+            },
+            child: AnimatedBuilder(
+              animation: _buttonScaleAnimation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _buttonScaleAnimation.value,
+                  child: Icon(Icons.account_circle),
+                );
+              },
+            ),
+          ),
           label: 'Profile',
         ),
       ],
-      onTap: (index) {
-        if (index == 0) {
-          Navigator.pushNamed(context, '/home');
-        } else if (index == 1) {
-          Navigator.pushNamed(context, '/profile');
-        }
-      },
     );
   }
 }
